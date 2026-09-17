a504e2f12b024266dc344935e14703236d65a041
Kalpana Sastry <sastry.kalpana@gmail.com>
2026-09-17 14:33:45 -0700

    Add: longest-palindromic-substring - submission-4

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-4.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String longestPalindrome(String s) {
+
+        int[] res = {0, 0};
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+
+        char[] chars = s.toCharArray();
+
+        // Base case 1 - single character
+        for(int i=0; i< len; i++){
+            dp[i][i] = true;
+        }
+        // Base case 2 - substring of length 2
+        for(int i=0; i< len-1; i++){
+            // check if the character are equal
+            dp[i][i+1] = chars[i] == chars[i+1];
+            if(dp[i][i+1]){
+                res = new int[]{i, i+1};
+            }
+        }
+
+        // Case 3 - substring greater than equal to length 3
+        for(int n = 3; n<=len; n++){
+            for(int i = 0; i <= len - n; i++){
+                int j = i + n - 1;
+                dp[i][j] = dp[i+1][j-1] && (chars[i] == chars[j]);
+                if(dp[i][j]){
+                    res = new int[]{i, j};
+                }
+            }
+        }
+        return s.substring(res[0], res[1]+1);
+    }
+}
